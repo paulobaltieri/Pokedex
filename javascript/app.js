995e3b112671a0e8a75c74b72138d85b4a38aae9
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2022-11-07 12:29:01 -0300

    configuração input

--- javascript/app.js
+++ javascript/app.js
@@ -2,14 +2,24 @@ const form = document.querySelector('.form')
 const inputPesquisar = document.querySelector('.inputPesquisar')
 const pokemonNumber = document.querySelector('.pokemonNumber')
 const pokemonName = document.querySelector('.pokemonName')
+const pokemonImage = document.querySelector('.pokemonImage')
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault()
     const pokemon = inputPesquisar.value
     const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const data = await pokeApi.json()
-    pokemonNumber.innerHTML = data.id
+
+    const pokemonId = data.id
+    const requestImg = new Request(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonId}.gif`)
+    fetch(requestImg)
+        .then((response) => response.blob())
+        .then((pBlob) => {
+            const objectURL = URL.createObjectURL(pBlob)
+            pokemonImage.src = objectURL
+        })
     pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
     console.log(data.name)
     console.log(data.id)
 })
