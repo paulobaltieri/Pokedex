3bd3f54b8ad7e3ac07d901c6ced4b76509b67302
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2022-11-10 09:31:57 -0300

    Adicionado a opção que informa o tipo do pokemon

--- javascript/app.js
+++ javascript/app.js
@@ -3,14 +3,36 @@ const inputPesquisar = document.querySelector('.inputPesquisar')
 const pokemonNumber = document.querySelector('.pokemonNumber')
 const pokemonName = document.querySelector('.pokemonName')
 const pokemonImage = document.querySelector('.pokemonImage')
+const btnPrevious = document.querySelector('.btn-prev')
+const pokemonTipo = document.querySelector('.inputTipo')
+const btnNext = document.querySelector('.btn-next')
+
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault()
     const pokemon = inputPesquisar.value
+
     const pokeApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const data = await pokeApi.json()
+    const pokemonType = data.types
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    const pokeType = (value) => {
+        for (let i = 0; i < value.length; i++) {
+            const element = value[i].type.name;
+            return element
+        }
+    }
+    console.log(pokeType(pokemonType))
+    const teste = pokeType(pokemonType)
+    pokemonTipo.innerHTML = teste
+    imgFunc(data)
 
-    const pokemonId = data.id
+})
+
+
+const imgFunc = (dados) => {
+    const pokemonId = dados.id
     const requestImg = new Request(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemonId}.gif`)
     fetch(requestImg)
         .then((response) => response.blob())
@@ -18,8 +40,5 @@ form.addEventListener('submit', async (event) => {
             const objectURL = URL.createObjectURL(pBlob)
             pokemonImage.src = objectURL
         })
-    pokemonName.innerHTML = data.name
-    pokemonNumber.innerHTML = data.id
-    console.log(data.name)
-    console.log(data.id)
-})
+
+}
